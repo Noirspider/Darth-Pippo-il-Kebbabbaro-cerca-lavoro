ef292922da290b5049f9c584b120ea946d66cbb9
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-23 15:16:56 +0100

    ok

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -20,3 +20,24 @@ export const fetchProfileAction = (idProfile) => async (dispatch) => {
     console.error("Errore nel fetch:", error.message);
   }
 };
+export const fetchPutProfileAction = (idProfile, newProfileObject) => async (dispatch) => {
+  try {
+    const response = await fetch("https://striveschool-api.herokuapp.com/api/profile/" + idProfile, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProfileObject),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      dispatch(setMyProfile(data));
+    } else {
+      throw new Error("Errore nel recupero dei risultati");
+    }
+  } catch (error) {
+    console.error("Errore nel fetch:", error.message);
+  }
+};
