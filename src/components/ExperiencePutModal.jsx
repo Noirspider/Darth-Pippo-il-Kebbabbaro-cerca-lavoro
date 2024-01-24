dbbe4a162d5223f00be580e0db2cf04a501c7007
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-24 16:15:25 +0100

    star guardian kassadin

--- src/components/ExperiencePutModal.jsx
+++ src/components/ExperiencePutModal.jsx
@@ -50,21 +50,21 @@ function ExperienceModal({ expData, show, handleClose }) {
         <Modal.Header closeButton>
           <Modal.Title>Experience Settings</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <Form>
+        <Form onSubmit={handleSaveChanges}>
+          <Modal.Body>
             <Form.Group controlId="formRole">
               <Form.Label>Role</Form.Label>
-              <Form.Control type="text" name="role" value={formData.role} onChange={handleInputChange} />
+              <Form.Control type="text" name="role" value={formData.role} onChange={handleInputChange} required />
             </Form.Group>
 
             <Form.Group controlId="formCompany">
               <Form.Label>Company</Form.Label>
-              <Form.Control type="text" name="company" value={formData.company} onChange={handleInputChange} />
+              <Form.Control type="text" name="company" value={formData.company} onChange={handleInputChange} required />
             </Form.Group>
 
             <Form.Group controlId="formArea">
               <Form.Label>Area</Form.Label>
-              <Form.Control type="text" name="area" value={formData.area} onChange={handleInputChange} />
+              <Form.Control type="text" name="area" value={formData.area} onChange={handleInputChange} required />
             </Form.Group>
 
             <Form.Group controlId="formDescription">
@@ -74,28 +74,35 @@ function ExperienceModal({ expData, show, handleClose }) {
                 name="description"
                 value={formData.description}
                 onChange={handleInputChange}
+                required
               />
             </Form.Group>
 
             <Form.Group controlId="formStartDate">
               <Form.Label>Start Date</Form.Label>
-              <Form.Control type="Date" name="startDate" value={formData.startDate} onChange={handleInputChange} />
+              <Form.Control
+                type="Date"
+                name="startDate"
+                value={formData.startDate}
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
 
             <Form.Group controlId="formEndDate">
               <Form.Label>End Date</Form.Label>
-              <Form.Control type="Date" name="endDate" value={formData.endDate} onChange={handleInputChange} />
+              <Form.Control type="Date" name="endDate" value={formData.endDate} onChange={handleInputChange} required />
             </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleSaveChanges}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button type="submit" variant="primary">
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
     </>
   );
